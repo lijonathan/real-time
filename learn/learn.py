ec9257906257ef85e6040ba6c9013dacd9541b67
Justin Marshall <jmarsha82@yahoo.com>
2020-03-01 22:23:22 -0600

    (JM) - Fixed imports for python script

--- learn/learn.py
+++ learn/learn.py
@@ -1,21 +1,13 @@
-from builtins import print
-# Check the versions of libraries
-# Python version
 import sys
-print('Python: {}'.format(sys.version))
-# scipy
 import scipy
-print('scipy: {}'.format(scipy.__version__))
-# numpy
 import numpy
-print('numpy: {}'.format(numpy.__version__))
-# matplotlib
 import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
-# pandas
 import pandas
-print('pandas: {}'.format(pandas.__version__))
-# scikit-learn
 import sklearn
+print('Python: {}'.format(sys.version))
+print('scipy: {}'.format(scipy.__version__))
+print('numpy: {}'.format(numpy.__version__))
+print('matplotlib: {}'.format(matplotlib.__version__))
+print('pandas: {}'.format(pandas.__version__))
 print('sklearn: {}'.format(sklearn.__version__))
-print("This line will be printed.")
+
