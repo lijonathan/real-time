c693608723f3a92141fcd5d11427e7736ff3ac38
Justin Marshall <jmarsha82@yahoo.com>
2020-03-22 13:53:23 -0500

    (JM) Added Bootstrap and Updated Sample Data

---
+++ src/web-app/misc/gloveLearning.py
@@ -0,0 +1,28 @@
+from sklearn.ensemble import RandomForestClassifier
+
+json_data = {"state":
+    {
+        "reported": {
+
+            "imu_z": -17.625,
+            "imu_y": 0.1875,
+            "imu_x": 251.625
+        }
+    }
+
+}
+y = ["a"]
+
+##l = json.dumps(json_data)
+# print(type(l))
+data = []
+for key, item in json_data["state"]["reported"].items():
+    data.append(item)
+
+x = [data]
+
+print(data)
+rf = RandomForestClassifier()
+rf.fit(x, y)
+
+# rf.fit(train_X, train_y)
