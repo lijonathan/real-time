0301ce283ae8e8e52e5f25c76c0fe70c32d99f10
Justin Marshall <jmarsha82@yahoo.com>
2020-04-20 16:14:17 -0500

    Cleaned up JS, script refreshing with cache issues.

--- web-app/public/js/index.js
+++ web-app/public/js/index.js
@@ -1,57 +1,11 @@
 
-function findA() {
+function findLetter(temp) {
     const numGenHeader = document.getElementById("letter-generator");
-    numGenHeader.innerText = "A";
+    numGenHeader.innerText = temp;
     const str = document.getElementById("output").innerText;
-    const tempValue = str.search(/a/g);  //to match letter to what I have
-    switcher(tempValue);
-}
-
-function findB() {
-    const numGenHeader = document.getElementById("letter-generator");
-    numGenHeader.innerText = "B";
-    const str = document.getElementById("output").innerText;
-    const tempValue = str.search(/b/g);  //to match letter to what I have
-    switcher(tempValue);
-}
-
-function findC() {
-    const numGenHeader = document.getElementById("letter-generator");
-    numGenHeader.innerText = "C";
-    const str = document.getElementById("output").innerText;
-    const tempValue = str.search(/c/g);  //to match letter to what I have
-    switcher(tempValue);
-}
-
-function findD() {
-    const numGenHeader = document.getElementById("letter-generator");
-    numGenHeader.innerText = "D";
-    const str = document.getElementById("output").innerText;
-    const tempValue = str.search(/d/g);  //to match letter to what I have
-    switcher(tempValue);
-}
-
-function findE() {
-    const numGenHeader = document.getElementById("letter-generator");
-    numGenHeader.innerText = "E";
-    const str = document.getElementById("output").innerText;
-    const tempValue = str.search(/e/g);  //to match letter to what I have
-    switcher(tempValue);
-}
-
-function findF() {
-    const numGenHeader = document.getElementById("letter-generator");
-    numGenHeader.innerText = "F";
-    const str = document.getElementById("output").innerText;
-    const tempValue = str.search(/f/g);  //to match letter to what I have
-    switcher(tempValue);
-}
-
-function findG() {
-    const numGenHeader = document.getElementById("letter-generator");
-    numGenHeader.innerText = "G";
-    const str = document.getElementById("output").innerText;
-    const tempValue = str.search(/g/g);  //to match letter to what I have
+    const lowerTemp = temp.toLowerCase();
+    const re = new RegExp(lowerTemp, 'g');
+    const tempValue = str.search(re);  //to match letter to what I have
     switcher(tempValue);
 }
 
