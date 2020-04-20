97c8a9e75b75b9ee2defe91ad0c6fd9e7e835984
Jmmanin <jmmanin@gmail.com>
2020-04-19 21:20:22 -0500

    Update demo cloud script
    Updated the cloud demo script to wait for a control topic before
    gathering glove data. Added comments. Change var and func names to
    match python standard.
    
    Moved certs folder to more central location.

--- learn/demo_cloud.py
+++ learn/demo_cloud.py
@@ -2,35 +2,90 @@
 * CSE520 Real-Time Systems
 * Cloud ML processing of data sent from glove
 * Jonathan Li
-*
-* Created with help from sample code from AWS
-* Random Forest Classifier
 '''
 
+####################
+# Import Libraries #
+####################
+
 # Import utility libs
+import json
+import os
 import time
 # Import AWS IoT Core libs
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import logging
+# Import Random Forest Classifier lib
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
-import json
-from os import listdir
-from os.path import isfile, join
-import os
-import sys
 
+############################
+# Helper Functions/Classes #
+############################
+
+# Custom MQTT message callback
+class CallbackContainer(object):
+
+    # Constructor
+    def __init__(self, client):
+        self._client = client
+        self._state = False
+        self._glove_data_buffer = []
+
+    # Control topic callback function
+    def control_callback(self, client, userdata, message):
+        # Contents of topic not important, receipt of topic alone is the trigger
+        self._state = True
+
+    # Glove data topic callback function
+    def glove_data_callback(self, client, userdata, message):
+        topic_contents = json.loads(message.payload.decode('utf-8'))
+
+        glove_data = []
+        glove_data.append(topic_contents['state']['reported']['imu_x'])
+        glove_data.append(topic_contents['state']['reported']['imu_y'])
+        glove_data.append(topic_contents['state']['reported']['imu_z'])
+        glove_data.append(topic_contents['state']['reported']['flex_index'])
+        glove_data.append(topic_contents['state']['reported']['flex_middle'])
+        glove_data.append(topic_contents['state']['reported']['flex_ring'])
+        glove_data.append(topic_contents['state']['reported']['flex_pinky'])
+        glove_data.append(topic_contents['state']['reported']['flex_thumb'])
+
+        self._glove_data_buffer.append(glove_data)
+
+        print(topic_contents)
+        print("\n\n\n")
+
+    # Accessor functions
+    def get_control_state(self):
+        return self._state
+
+    def get_glove_data_buffer(self):
+        return self._glove_data_buffer
+
+    # Mutator functions
+    def reset_control_state(self):
+        self._state = False
+
+    def clear_glove_data_buffer(self):
+        self._glove_data_buffer = []
+
+####################
+# Initialize Cloud #
+####################
+
+# Initialize Random Forest Classifier
 X = []
 Y = []
 
-onlyfiles = [f for f in listdir("./training_data") if isfile(join("./training_data", f))]
+## Get training data files
+only_files = [f for f in os.listdir("./training_data") if os.path.isfile(os.path.join("./training_data", f))]
 
-for i in range(0, len(onlyfiles)):
-    onlyfiles[i] = os.path.join("./training_data", onlyfiles[i])
+for i in range(0, len(only_files)):
+    only_files[i] = os.path.join("./training_data", only_files[i])
 
-for i in range(0, len(onlyfiles)):
-    with open(onlyfiles[i]) as data_file:
+## Read training data
+for i in range(0, len(only_files)):
+    with open(only_files[i]) as data_file:
         for line in data_file:
             line = line.strip("\n")
             x_pt = []
@@ -40,140 +95,101 @@ for i in range(0, len(onlyfiles)):
             for k in range(0, len(data_pts)):
                 x_pt.append(float(data_pts[k]))
 
-            
             X.append(x_pt)
             Y.append(result)
-            
 
-print(X)
-print(Y)
-
-
+## Create Random Forest Classifier object
 rf = RandomForestClassifier(max_depth=15, min_samples_leaf=1, min_samples_split = 4, n_estimators = 1000)
 
+## Build random forest from training data
 rf.fit(X, Y)
 
-
-
-class CallbackContainer(object):
-
-    def __init__(self, client):
-        self._client = client
-        self.imu_orient_z = 0
-        self.imu_orient_x = 0
-        self.imu_orient_y = 0
-        self.flex_index = 0
-        self.flex_middle = 0
-        self.flex_ring = 0
-        self.flex_pinky = 0
-        self.flex_thumb = 0
-
-
-    # Custom MQTT message callback
-    def customCallback(self, client, userdata, message):
-        topicContents = json.loads(message.payload.decode('utf-8'))
-        self.imu_orient_x = topicContents['state']['reported']['imu_x']
-        self.imu_orient_y = topicContents['state']['reported']['imu_y']
-        self.imu_orient_z = topicContents['state']['reported']['imu_z']
-
-        self.flex_index = topicContents['state']['reported']['flex_index']
-        self.flex_middle = topicContents['state']['reported']['flex_middle']
-        self.flex_ring = topicContents['state']['reported']['flex_ring']
-        self.flex_pinky = topicContents['state']['reported']['flex_pinky']
-        self.flex_thumb = topicContents['state']['reported']['flex_thumb']
-
-
-        print(topicContents)
-        print("\n\n\n")
-
-    def getx(self):
-        return self.imu_orient_x
-
-    def gety(self):
-        return self.imu_orient_y
-
-    def getz(self):
-        return self.imu_orient_z
-
-    def get_flex_index(self):
-        return self.flex_index
-
-    def get_flex_middle(self):
-        return self.flex_middle
-
-    def get_flex_ring(self):
-        return self.flex_ring
-
-    def get_flex_pinky(self):
-        return self.flex_pinky
-
-    def get_flex_thumb(self):
-        return self.flex_thumb
-
-
-
-# Connection settings
+# Setup AWS IoT
+## Configure AWS IoT connection settings
 host = "an91x6ytmr3ss-ats.iot.us-east-2.amazonaws.com"
-rootCAPath = "../glove/certs/root-CA.crt"
-certificatePath = "../glove/certs/2db4660fce-certificate.pem.crt"
-privateKeyPath = "../glove/certs/2db4660fce-private.pem.key"
+root_CA_path = "../certs/root-CA.crt"
+certificate_path = "../certs/2db4660fce-certificate.pem.crt"
+private_key_path = "../certs/2db4660fce-private.pem.key"
 port = 8883
-clientId = "cloud_Ec2"
-sensorDataTopic = "$aws/things/processed_data/shadow/update"
-controlTopic = "$aws/things/sensor_glove/shadow/update"
+client_id = "cloud_Ec2"
+cloud_control_topic = "$aws/things/cloud_control/shadow/update"
+result_topic = "$aws/things/processed_data/shadow/update"
+glove_control_topic = "$aws/things/glove_control/shadow/update"
+glove_data_topic = "$aws/things/sensor_glove/shadow/update"
 
-# Init AWSIoTMQTTClient
-myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-myAWSIoTMQTTClient.configureEndpoint(host, port)
-myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+## Configure AWSIoTMQTTClient connection settings
+my_iot_client = AWSIoTMQTTClient(client_id)
+my_iot_client.configureEndpoint(host, port)
+my_iot_client.configureCredentials(root_CA_path, private_key_path, certificate_path)
 
-# AWSIoTMQTTClient connection configuration
-myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+## Configure AWS IoT logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.ERROR)
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stream_handler.setFormatter(formatter)
+logger.addHandler(streamHandler)
 
-# Connect to AWS IoT
-myAWSIoTMQTTClient.connect()
+## Init AWSIoTMQTTClient
+my_iot_client.configureAutoReconnectBackoffTime(1, 32, 20)
+my_iot_client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+my_iot_client.configureDrainingFrequency(2)  # Draining: 2 Hz
+my_iot_client.configureConnectDisconnectTimeout(10)  # 10 sec
+my_iot_client.configureMQTTOperationTimeout(5)  # 5 sec
 
-# Subscribe to control sensorDataTopic
-myCallbackContainer = CallbackContainer(myAWSIoTMQTTClient)
-myAWSIoTMQTTClient.subscribe(controlTopic, 1, myCallbackContainer.customCallback)
+## Connect to AWS IoT
+my_iot_client.connect()
+
+## Subscribe to control topic
+my_callback_container = CallbackContainer(my_iot_client)
+my_iot_client.subscribe(cloud_control_topic, 1, my_callback_container.control_callback)
+
+## Subscribe to glove data topic
+my_callback_container = CallbackContainer(my_iot_client)
+my_iot_client.subscribe(glove_data_topic, 1, my_callback_container.glove_data_callback)
+
 time.sleep(2)
 
+########################
+# Main Processing Loop #
+########################
 
-# Publish to sensor data sensorDataTopic when start sensorDataTopic is received until control sensorDataTopic says stop
 while True:
+    # Get sensor data for prediction once control topic is received
+    if (my_callback_container.get_control_state()):
+        # Reset control state as request is being processed
+        my_callback_container.reset_control_state()
 
+        # Reset glove data buffer
+        my_callback_container.clear_glove_data_buffer()
 
-    X_received = []
-    data_pt = []
-    data_pt.append(myCallbackContainer.getx())
-    data_pt.append(myCallbackContainer.gety())
-    data_pt.append(myCallbackContainer.getz())
+        # Publish glove start command
+        message = {}
+        message['state'] = {}
+        message['state']['reported'] = {}
+        message['state']['reported']['command'] = 'start'
+        message_json = json.dumps(message)
+        my_iot_client.publish(glove_control_topic, message_json, 1)
 
-    data_pt.append(myCallbackContainer.get_flex_index())
-    data_pt.append(myCallbackContainer.get_flex_middle())
-    data_pt.append(myCallbackContainer.get_flex_ring())
-    data_pt.append(myCallbackContainer.get_flex_pinky())
-    data_pt.append(myCallbackContainer.get_flex_thumb())
+        # Wait for buffer to fill with glove data
+        time.sleep(5)
 
-    print(data_pt)
-    sum_vals = 0
-    for val in data_pt:
-        sum_vals += sum_vals + val
+        # Publish glove stop command
+        message = {}
+        message['state'] = {}
+        message['state']['reported'] = {}
+        message['state']['reported']['command'] = 'stop'
+        message_json = json.dumps(message)
+        my_iot_client.publish(glove_control_topic, message_json, 1)
 
-    send = True
-    if sum_vals == 0:
-        send = False
+        # Get random forest classifier's predictions
+        predictions = rf.predict(my_callback_container.get_glove_data_buffer())
 
+        #TO DO: GET MODE FROM LIST OF PREDICTIONS
+        send_result = predictions[0]
 
-    X_received.append(data_pt)
-    res = rf.predict(X_received)
-    
-    send_result = res[0]
-    if send:
-        myAWSIoTMQTTClient.publish(sensorDataTopic, send_result, 1)
+        # Publish result
+        my_iot_client.publish(result_topic, send_result, 1)
         print(send_result)
+
     time.sleep(1)
