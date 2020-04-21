fa0e3807e848135e9d27f4b584b69975533b3cbc
Justin Marshall <jmarsha82@yahoo.com>
2020-04-21 00:04:21 -0500

    Updated

--- web-app/public/js/window.js
+++ web-app/public/js/window.js
@@ -1,68 +1,25 @@
-// import {PythonShell} from 'python-shell';
-let {PythonShell} = require('python-shell')
 
-function sendOnCommand() {
 
-    PythonShell.run('../../../glove/glove_start.py', null, function (err) {
-        if (err) throw err;
+
+require(['python-shell'], function sendOnCommand() {
+    const myPythonScriptPath = '../../../glove/glove_start.py';
+
+// Use python shell
+    const PythonShell = require('python-shell');
+    var pyshell = new PythonShell(myPythonScriptPath);
+
+    pyshell.on('message', function (message) {
+        // received a message sent from the Python script (a simple "print" statement)
+        console.log(message);
+    });
+
+// end the input stream and allow the process to exit
+    pyshell.end(function (err) {
+        if (err) {
+            throw err;
+        }
+        ;
+
         console.log('finished');
     });
-
-    // var xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         // Typical action to be performed when the document is ready:
-    //     }
-    // };
-    // xhttp.open("GET", "../index.php", true);
-    // xhttp.send();
-    // // const execSync = require('child_process').execSync;
-    // // const output = execSync('python ../../../glove/glove_start.py')
-    // // $.ajax({
-    // //     type: "POST",
-    // //     url: "../../../glove/glove_start.py",
-    // //     async: false,
-    // // });
-    // // const textAreaTag = document.getElementById("output");
-    // // textAreaTag.innerHTML = output.responseText
-    // $.ajax({
-    //     type: "POST",
-    //     url: "../../../glove/glove_start.py",
-    //     data: { param: ''}
-    // }).done(function( o ) {
-    //     // do something
-    //     console.log('We Done')
-    // });
-
-
-}
-
-// window.setInterval(function () {
-//     const rawFile = new XMLHttpRequest();
-//     rawFile.open("GET", 'hand_data.txt', true);
-//     rawFile.onreadystatechange = function () {
-//         if (rawFile.readyState === 4) {
-//             if (rawFile.status === 200 || rawFile.status == 0) {
-//                 const allText = rawFile.responseText;
-//                 const textAreaTag = document.getElementById("output");
-//                 textAreaTag.innerHTML = allText
-//             }
-//         }
-//     };
-//     rawFile.send(null);
-// }, 2000);
-
-// define(function (require) {
-//     var child_process = require('child_process');
-// });
-
-function sendOffCommand() {
-    const jqXHR = $.ajax({
-        type: "POST",
-        url: "../../../glove/glove_stop.py",
-        async: true,
-    });
-    const textAreaTag = document.getElementById("output");
-    textAreaTag.innerHTML = jqXHR.responseText
-
-}
+});
