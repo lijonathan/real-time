f868c11cac3c479128d26f09cda1ff3aadf6adb7
Jmmanin <jmmanin@gmail.com>
2020-04-22 19:08:53 -0500

    Add backend subscriber

--- web-app/backend/backend.py
+++ web-app/backend/backend.py
@@ -1,3 +1,9 @@
+'''
+* CSE520 Real-Time Systems
+* UI Backend flask server
+* Jeremy Manin, Justin Marshall
+'''
+
 from flask import Flask
 from flask_cors import CORS
 import threading
@@ -7,9 +13,43 @@ import time
 import json
 import backend_sub
 
-
+# Flask setup
 app = Flask(__name__)
 CORS(app)
+
+# AWS IoT Setup
+## Connection settings
+host = "an91x6ytmr3ss-ats.iot.us-east-2.amazonaws.com"
+rootCAPath = "../../certs/root-CA.crt"
+certificatePath = "../../certs/2db4660fce-certificate.pem.crt"
+privateKeyPath = "../../certs/2db4660fce-private.pem.key"
+port = 8883
+clientId = "back_end_pub"
+controlTopic = "$aws/things/cloud_control/shadow/update"
+
+## Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.ERROR)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+## Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureEndpoint(host, port)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+## AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+## Connect to AWS IoT
+myAWSIoTMQTTClient.connect()
+time.sleep(2)
 
 class sub_thread (threading.Thread):
    def __init__(self):
@@ -21,43 +61,10 @@ class sub_thread (threading.Thread):
 def start_subscribe():
     my_sub_thread = sub_thread()
     my_sub_thread.start()
-    return('Subscription service started')
+    return('Subscription thread started')
 
 @app.route("/cloud-start")
 def start_cloud():
-    # Connection settings
-    host = "an91x6ytmr3ss-ats.iot.us-east-2.amazonaws.com"
-    rootCAPath = "../../certs/root-CA.crt"
-    certificatePath = "../../certs/2db4660fce-certificate.pem.crt"
-    privateKeyPath = "../../certs/2db4660fce-private.pem.key"
-    port = 8883
-    clientId = "back_end"
-    controlTopic = "$aws/things/cloud_control/shadow/update"
-
-    # Configure logging
-    logger = logging.getLogger("AWSIoTPythonSDK.core")
-    logger.setLevel(logging.ERROR)
-    streamHandler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    streamHandler.setFormatter(formatter)
-    logger.addHandler(streamHandler)
-
-    # Init AWSIoTMQTTClient
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-    myAWSIoTMQTTClient.configureEndpoint(host, port)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
-
-    # AWSIoTMQTTClient connection configuration
-    myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-    myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-    myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-    myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-    myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-
-    # Connect to AWS IoT
-    myAWSIoTMQTTClient.connect()
-    time.sleep(2)
-
     # Build and publish control topic telling cloud to start
     message = {}
     message['state'] = {}
--- web-app/backend/backend_sub.py
+++ web-app/backend/backend_sub.py
@@ -1,12 +1,79 @@
 '''
 * CSE520 Real-Time Systems
 * UI Backend subscription service
-* Jeremy Manin
+* Jeremy Manin, Justin Marshall
 '''
 
+# Import utility libs
+import time
+import os
+# Import AWS IoT Core libs
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import json
+
+# Custom MQTT message callback
+class CallbackContainer(object):
+
+    # Constructor
+    def __init__(self, client):
+        self._client = client
+
+    # Processed_Data topic callback function
+    def processed_data_callback(self, client, userdata, message):
+        # Write received data to file
+        with open ("hand_data.txt", 'w') as data_file:
+            write_line = str(message.payload)
+            write_line = write_line.strip("b")
+            data_file.write(write_line)
+            data_file.write("\n")
+        print('Backend Sub Received: ' + str(message.payload))
+
 def start_sub():
-    print('Hello World!')
-    # Add subscription code
+    # Setup AWS IoT
+    ## Configure AWS IoT connection settings
+    host = "an91x6ytmr3ss-ats.iot.us-east-2.amazonaws.com"
+    root_CA_path = "../../certs/root-CA.crt"
+    certificate_path = "../../certs/2db4660fce-certificate.pem.crt"
+    private_key_path = "../../certs/2db4660fce-private.pem.key"
+    port = 8883
+    client_id = "back_end_sub"
+    data_topic = "$aws/things/processed_data/shadow/update"
+
+    ## Configure AWS IoT logging
+    logger = logging.getLogger("AWSIoTPythonSDK.core")
+    logger.setLevel(logging.ERROR)
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    ## Init AWSIoTMQTTClient
+    my_iot_client = AWSIoTMQTTClient(client_id)
+    my_iot_client.configureEndpoint(host, port)
+    my_iot_client.configureCredentials(root_CA_path, private_key_path, certificate_path)
+
+    ## Configure AWSIoTMQTTClient connection settings
+    my_iot_client.configureAutoReconnectBackoffTime(1, 32, 20)
+    my_iot_client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    my_iot_client.configureDrainingFrequency(2)  # Draining: 2 Hz
+    my_iot_client.configureConnectDisconnectTimeout(10)  # 10 sec
+    my_iot_client.configureMQTTOperationTimeout(5)  # 5 sec
+
+    ## Connect to AWS IoT
+    my_iot_client.connect()
+
+    ## Subscribe to control topic
+    my_callback_container = CallbackContainer(my_iot_client)
+    my_iot_client.subscribe(data_topic, 1, my_callback_container.processed_data_callback)
+
+    time.sleep(2)
+
+    print('\nBackend subscriber started\n')
+
+    # Infinitely sleep to keep subscribing
+    while True:
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     start_sub()
